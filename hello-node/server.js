a41a270af3a7e9b85755fb5da392e3f1ba736ef1
zhangpeihe <dream_zph@163.com>
2017-01-16 16:13:41 +0800

    Update server.js

--- hello-node/server.js
+++ hello-node/server.js
@@ -2,17 +2,15 @@ const http = require('http');
 const handleRequest = (request, response) => {
   console.log('Received request for URL: ' + request.url);
   var os=require('os'),
-  iptable={},
-  ifaces=os.networkInterfaces();
-  for (var dev in ifaces) {
-    ifaces[dev].forEach(function(details,alias){
-      if (details.family=='IPv4') {
-        iptable[dev+(alias?':'+alias:'')]=details.address;
-     }
-    });
-  }
+  var IPv4,hostName;  
+  hostName=os.hostname();  
+  for(var i=0;i<os.networkInterfaces().en0.length;i++){  
+    if(os.networkInterfaces().en0[i].family=='IPv4'){  
+        IPv4=os.networkInterfaces().en0[i].address;  
+    }  
+  }  
   response.writeHead(200);
-  response.end('Hello World!'+iptable);
+  response.end('Hello World!'+IPv4);
 };
 const www = http.createServer(handleRequest);
 www.listen(9080);
